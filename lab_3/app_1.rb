9cc39fc8ae1508687760cf1792912047a81176a6
Александр <Fybeer@yandex.ru>
2021-10-20 02:20:05 +0300

    end

--- lab_3/app_1.rb
+++ lab_3/app_1.rb
@@ -1,33 +1,47 @@
 require 'fileutils'
+if File.zero?("list.txt")
+  abort "Пожалуйста наполните файл list.txt"
+  else
 new_array = File.readlines("list.txt").sort
-File.open("list.txt", "w") do |file|
-	new_array.each {|n| file.puts(n)}
+File.open("list.txt","w") do |file|
+  new_array.each {|n| file.puts(n)}
 end
-@stud_Data = File.read("list.txt").split("\n")
+end
+@stud_Data=File.read("list.txt").split("\n")
+
+def putOnScreen
+  File.open("results.txt") do |file|
+    file.readlines().each { |line|
+      puts line
+    }
+  end
+end
 
 def find
-	puts "Введите возраст"
-	value=gets.chomp
-	stud=@stud_Data
-	stud.each do |x|
-	  if x[value]
-		 File.write("results.txt", "#{x}\n", mode: "a")
-	  end
-	end
- end
- def sort
-	new_array = File.readlines("results.txt").sort
-	File.open("results.txt","w") do |file|
-	  new_array.each {|n| file.puts(n)}
-	end
- end
- loop do
-	puts "Если хотите добавить сотрудника пишите '1' если хотите выйти напишите '-1', отсортировать массив 2"
-	value=gets
-	case value
-	when "1\n" then find
-	when "2\n" then sort
-	break if value.to_i==-1
-	break if FileUtils.compare_file('list.txt','results.txt')==true
-	end
- end
+  puts "Введите возраст"
+  value=gets.chomp
+  stud=@stud_Data
+  stud.each do |x|
+    if x[value]
+      File.write("results.txt", "#{x}\n", mode: "a")
+    end
+  end
+end
+
+
+
+loop do
+
+  puts "Добавить сотрудника - 'write' ,results.txt - 'result', выйти - 'exit' "
+  value=gets
+  case value
+  when "write\n" then find
+  when "result\n" then putOnScreen
+  when "exit\n" then abort
+
+
+  end
+
+
+
+  end
