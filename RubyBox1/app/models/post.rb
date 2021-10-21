7c504a00587eb30373d8617a92ee26a91dd12ad5
Александр <43615653+Fyreed@users.noreply.github.com>
2021-10-21 17:42:43 +0300

    Delete RubyBox1 directory

--- RubyBox1/app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ApplicationRecord
-  has_many :marks, dependent: :destroy
-
-  validates :title, presence: true, length: { minimum: 5 }
-  validates :description, presence: true, length: { minimum: 10 }
-end
