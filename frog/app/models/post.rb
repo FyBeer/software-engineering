e55c8d40204cb9317bf994bac8c6dd951294302a
Александр <Fybeer@yandex.ru>
2021-10-22 11:00:35 +0300

    end

--- frog/app/models/post.rb
+++ frog/app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
 	has_many :marks, dependent: :destroy
  
-	validates :title, presence: true, length: { minimum: 5 }
-	validates :description, presence: true, length: { minimum: 10 }
+	validates :title, presence: true, length: { maximum: 250 }
+	validates :description, presence: true, length: { maximum: 500 }
  end
  
